35a7c5cdfd5016cc9118f66dab8d3910cdc20091
Mehwa <carpediem622@gmail.com>
2019-02-08 17:13:18 +0900

    BJ14891 Gear BJ2184 Robot Simulation

---
+++ Algorithm_Study_KPark/BJ14891_Gear/BJ14891_Gear.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int k;
+int c[4][8];
+bool cr[4] = { false };
+vector<pair<int, int> > r;
+
+void input() {
+	for (int i = 0; i < 4; i++) {
+		char line[9];
+		cin >> line;
+		for (int j = 0; j < 8; j++)
+			c[i][j] = line[j] - '0';
+	}
+	cin >> k;
+	for (int i = 0; i < k; i++) {
+		int n = 0, d = 0;
+		cin >> n >> d;
+		r.push_back(make_pair(n-1, d));
+	}
+}
+
+void printInput() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 8; j++) {
+			cout << c[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+	/*cout << k << endl;
+	for (int i = 0; i < k; i++) {
+		cout << r[i].first << " " << r[i].second << endl;
+	}*/
+}
+
+void rotateCircle(int n, int d) {
+	int prev = 0, cur = 0;
+	if (d == 1) {
+		prev = c[n][7];
+		for (int i = 0; i < 8; i++) {
+			cur = c[n][i];
+			c[n][i] = prev;
+			prev = cur;
+		}
+	}
+	else {
+		prev = c[n][0];
+		for (int i = 7; i > -1; i--) {
+			cur = c[n][i];
+			c[n][i] = prev;
+			prev = cur;
+		}
+	}
+}
+
+void rotateAll(int n, int d) {
+	int lc = n - 1;
+	int rc = n + 1;
+	cr[n] = true;
+	if (lc > -1 && c[lc][2] != c[n][6] && !cr[lc])
+		rotateAll(lc, -d);
+	if (rc < 4 && c[rc][6] != c[n][2] && !cr[rc])
+		rotateAll(rc, -d);
+	rotateCircle(n, d);
+}
+
+int score() {
+	return (c[0][0] + c[1][0] * 2 + c[2][0] * 4 + c[3][0] * 8);
+}
+
+int main()
+{
+	input();
+	//printInput();
+
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < 4; j++)
+			cr[j] = false;
+		rotateAll(r[i].first, r[i].second);
+		//printInput();
+	}
+	cout << score() << endl;
+}
---
+++ Algorithm_Study_KPark/BJ2174_RobotSimulation/BJ2174_RobotSimulation.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define OK 0
+#define WALLCRASH 1
+#define ROBOTCRASH 2
+
+int A, B, N, M;
+int map[102][102] = { 0 }; //robot location map
+vector <pair<int, int> > r; //robot location
+int dir[102] = { 0 }; //robot direction
+int dx[4] = { 0, 1, 0, -1 }; //N, E, S, W
+int dy[4] = { 1, 0, -1, 0 };
+typedef struct COMMAND {
+	int rid;
+	int n;
+	char cmd;
+};
+COMMAND cmdList[102];
+
+void input() {
+	cin >> A >> B;
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		int x = 0, y = 0;
+		char d;
+		cin >> x >> y >> d;
+		map[x][y] = i;
+		r.push_back(make_pair(x, y));
+		if (d == 'N')
+			dir[i] = 0;
+		else if (d == 'E')
+			dir[i] = 1;
+		else if (d == 'S')
+			dir[i] = 2;
+		else //'W'
+			dir[i] = 3;
+	}
+	for (int i = 0; i < M; i++) {
+		cin >> cmdList[i].rid >> cmdList[i].cmd >> cmdList[i].n;
+	}
+}
+
+void rotateLeft(int rid) {
+	dir[rid] = dir[rid] - 1;
+	if (dir[rid] < 0)
+		dir[rid] = 3;
+}
+void rotateRight(int rid) {
+	dir[rid] = (dir[rid] + 1) % 4;
+}
+
+int moveForward(int rid) {
+	int cx = r[rid - 1].first;
+	int cy = r[rid - 1].second;
+	int nx = cx + dx[dir[rid]];
+	int ny = cy + dy[dir[rid]];
+	if (nx < 1 || nx > A || ny < 1 || ny > B) {
+		cout << "Robot " << rid << " crashes into the wall" << endl;
+		return WALLCRASH;
+	}
+	else if (map[nx][ny] > 0 ) {
+		cout << "Robot " << rid << " crashes into robot " << map[nx][ny] << endl;
+		return ROBOTCRASH;
+	}
+	map[nx][ny] = rid;
+	map[cx][cy] = 0;
+	r[rid - 1].first = nx;
+	r[rid - 1].second = ny;
+	return OK;
+}
+
+int move(int rid, char cmd) {
+	int rt = OK;
+	if (cmd == 'L')
+		rotateLeft(rid);
+	else if (cmd == 'R')
+		rotateRight(rid);
+	else  // 'F'
+		rt = moveForward(rid);
+	return rt;
+}
+
+int main()
+{
+	input();
+	int rt = OK;
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < cmdList[i].n; j++) {
+			rt = move(cmdList[i].rid, cmdList[i].cmd);
+			if (rt != OK)
+				break;
+		}
+		if (rt != OK)
+			break;
+	}
+	if (rt == OK)
+		cout << "OK" << endl;
+}
