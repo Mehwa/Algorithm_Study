53ac5ea7b464236741a8bc6b4498e889c7bb76e9
Mehwa <carpediem622@gmail.com>
2019-02-08 17:24:07 +0900

    working-on problems

---
+++ Algorithm_Study_KPark/BJ2842_PostOfficer/BJ2842_PostOfficer.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int N, px, py;
+int minH = 987654321, maxH = 0;
+int housemap[52][52] = { false };
+int hmap[52][52] = { 0 };
+int cost[52][52] = { -1 };
+bool visited[52][52] = { false };
+int dx[8] = { 1, -1, 0, 0 , 1, 1, -1, -1}; //R, L, U, D, RU, RD, LD, LU
+int dy[8] = { 0, 0, -1, 1, -1, 1, 1, -1};
+vector<pair<int, int> > house;
+
+void input() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		char line[52];
+		cin >> line;
+		for (int j = 0; j < N; j++) {
+			if (line[j] == 'P') {
+				px = i + 1;
+				py = j + 1;
+			}
+			else if (line[j] == 'K') {
+				house.push_back(make_pair(i + 1, j + 1));
+				housemap[i + 1][j + 1] = true;
+			}
+		}
+	}
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			int h;
+			cin >> h;
+			hmap[i][j] = h;
+		}
+	}
+}
+
+void printInput() {
+	cout << "px:" << px << " py:" << py << endl;
+	for (int i = 0; i < house.size(); i++)
+		cout << house[i].first << " " << house[i].second << endl;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cout << hmap[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void initMinMax() {
+	for (int i = 0; i < house.size(); i++) {
+		int h = hmap[house[i].first][house[i].second];
+		if (h < minH)
+			minH = h;
+		if (h > maxH)
+			maxH = h;
+	}
+}
+
+void initCost() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (hmap[i][j] < minH)
+				cost[i][j] = minH - hmap[i][j];
+			else if (hmap[i][j] > maxH)
+				cost[i][j] = hmap[i][j] - maxH;
+			else
+				cost[i][j] = 0;
+		}
+	}
+}
+
+bool comp(pair<int, int> a, pair<int, int> b) {
+	return (cost[a.first][a.second] < cost[b.first][b.second]);
+}
+
+int visitHouse() {
+	int visitedHouse = 0;
+	int maxCost = 0;
+	queue<pair<int, int> > q;
+	q.push(make_pair(px, py));
+	visited[px][py] = true;
+	while (!q.empty()) {
+		pair<int, int> cur = q.front();
+		q.pop();
+		if (housemap[cur.first][cur.second]) {
+			visitedHouse++;
+			if (cost[cur.first][cur.second] > maxCost)
+				maxCost = cost[cur.first][cur.second];
+			if (visitedHouse == house.size())
+				break;
+		}
+		vector<pair<int, int> > d;
+		for (int i = 0; i < 8; i++) {
+			int nx = cur.first + dx[i];
+			int ny = cur.second + dy[i];
+			if (0 < nx && nx <= N && 0 < ny && ny <= N && !visited[nx][ny])
+				d.push_back(make_pair(nx, ny));
+		}
+		sort(d.begin(), d.end(), comp);
+		for (int i = 0; i < d.size(); i++) {
+			q.push(make_pair(d[i].first, d[i].second));
+			visited[d[i].first][d[i].second] = true;
+			if (cost[cur.first][cur.second] > cost[d[i].first][d[i].second])
+				cost[d[i].first][d[i].second] = cost[cur.first][cur.second];
+		}
+	}
+	return maxCost;
+}
+
+int main()
+{
+	input();
+	//printInput();
+	initMinMax();
+	initCost();
+	int c = visitHouse();
+	cout << maxH - minH + c;
+}
---
+++ Algorithm_Study_KPark/BJ5022_ConnectLine/BJ5022_ConnectLine.cpp
@@ -0,0 +1,144 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int N, M, a1x, a1y, a2x, a2y, b1x, b1y, b2x, b2y;
+bool map[101][101] = { true };
+bool visited[101][101] = { false };
+int dist[101][101] = { 0 };
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+
+void input() {
+	cin >> N >> M;
+	cin >> a1x >> a1y;
+	cin >> a2x >> a2y;
+	cin >> b1x >> b1y;
+	cin >> b2x >> b2y;
+	for (int i = 0; i <= N; i++) {
+		for (int j = 0; j <= M; j++) {
+			map[i][j] = true;
+		}
+	}
+	map[a1x][a1y] = false;
+	map[a2x][a2y] = false;
+	map[b1x][b1y] = false;
+	map[b2x][b2y] = false;
+}
+
+void printInput() {
+	/*cout << N << " " << M << endl;
+	cout << a1x << " " << a1y << endl;
+	cout << a2x << " " << a2y << endl;
+	cout << b1x << " " << b1y << endl;
+	cout << b2x << " " << b2y << endl;*/
+	
+	for (int j = 0; j <= M; j++) {
+		for (int i = 0; i <= N; i++) {
+			cout << map[i][j];
+		}
+		cout << endl;
+	}
+	for (int j = 0; j <= M; j++) {
+		for (int i = 0; i <= N; i++) {
+			cout << dist[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void init() {
+	for (int i = 0; i <= N; i++) {
+		for (int j = 0; j <= M; j++) {
+			visited[i][j] = false;
+			dist[i][j] = 0;
+		}
+	}
+}
+
+void initAll() {
+	for (int i = 0; i <= N; i++) {
+		for (int j = 0; j <= M; j++) {
+			map[i][j] = true;
+			visited[i][j] = false;
+			dist[i][j] = 0;
+		}
+	}
+}
+
+int connectLineBFS(int startx, int starty, int endx, int endy) {
+	queue<pair<int, int> > q;
+	q.push(make_pair(startx, starty));
+	visited[startx][starty] = true;
+	while (!q.empty()) {
+		pair<int, int> cur = q.front();
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nx = cur.first + dx[i];
+			int ny = cur.second + dy[i];
+			if (nx == endx && ny == endy) {
+				dist[nx][ny] = dist[cur.first][cur.second] + 1;
+				return dist[nx][ny];
+			}
+			if (map[nx][ny] && !visited[nx][ny] && 0 <= nx && nx <= N && 0 <= ny && ny <= M) {
+				q.push(make_pair(nx, ny));
+				visited[nx][ny] = true;
+				dist[nx][ny] = dist[cur.first][cur.second] + 1;
+			}
+		}
+	}
+	return -1; //impossible
+}
+
+void updateMap(int startx, int starty, int endx, int endy, int distance) {
+	int curx = endx;
+	int cury = endy;
+	while (distance > 0) {
+		map[curx][cury] = false;
+		for (int i = 0; i < 4; i++) {
+			int nx = curx + dx[i];
+			int ny = cury + dy[i];
+			if (map[nx][ny] && dist[nx][ny] == distance - 1 && 0 <= nx && nx <= N && 0 <= ny && ny <= M) {
+				distance--;
+				curx = nx;
+				cury = ny;
+				break;
+			}
+		}
+		if (distance == 1)
+			break;
+	}
+}
+
+
+int main()
+{
+	int dist1 = 0, dist2 = 0, dist3 = 0, dist4 = 0, sum1 = 0, sum2 = 0;
+	input();
+	
+	dist1 = connectLineBFS(a1x, a1y, a2x, a2y);
+	updateMap(a1x, a1y, a2x, a2y, dist1);
+	init();
+	dist2 = connectLineBFS(b1x, b1y, b2x, b2y);
+
+	sum1 = dist1 + dist2;
+
+	initAll();
+
+	dist3 = connectLineBFS(b1x, b1y, b2x, b2y);
+	updateMap(b1x, b1y, b2x, b2y, dist3);
+	init();
+	dist4 = connectLineBFS(a1x, a1y, a2x, a2y);
+
+	sum2 = dist3 + dist4;
+
+	if ((0 < dist1 && 0 < dist2) || (0 < dist3 && 0 < dist4)) {
+		if (sum1 > sum2)
+			cout << sum1 << endl;
+		else
+			cout << sum2 << endl;
+	}
+	else
+		cout << "IMPOSSIBLE" << endl;
+}
