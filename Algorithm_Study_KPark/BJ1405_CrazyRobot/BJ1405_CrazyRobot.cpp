bf98aa84fd7fcfb5ddb4c591f0ebf45eade9bee0
Mehwa <carpediem622@gmail.com>
2019-02-11 00:06:55 +0900

    BJ1405_CrazyRobot

---
+++ Algorithm_Study_KPark/BJ1405_CrazyRobot/BJ1405_CrazyRobot.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int N;
+double p[4]; //pe, pw, ps, pn
+bool visited[30][30];
+int dx[4] = { 1, -1, 0, 0 }; //E, W, S, N
+int dy[4] = { 0, 0, -1, 1 };
+
+void input() {
+	cin >> N;
+	for (int i = 0; i < 4; i++) {
+		int x;
+		cin >> x;
+		p[i] = x / (double)100;
+	}
+}
+
+double dfs(int cx, int cy, int depth) {
+	if (depth == N) return 1.0;
+	visited[cx][cy] = true;
+	double result = 0.0;
+	for (int i = 0; i < 4; i++) {
+		int nx = cx + dx[i];
+		int ny = cy + dy[i];
+		if (visited[nx][ny]) continue;
+		result += p[i] * dfs(nx, ny, depth+1);
+	}
+	visited[cx][cy] = false;
+	return result;
+}
+
+int main()
+{
+	input();
+	printf("%.10lf\n", dfs(14, 14, 0));
+}
